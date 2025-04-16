9c65ab11d053ad7cb07ff27f6aa739aef5ae3d64
jeliaaa <julia.aleqsandre@gmail.com>
2025-04-17 00:57:03 +0400

    /

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',  // Listen on all network interfaces
+    port: 5173,        // You can keep your desired port
+    open: true,         // Automatically open the app in the browser
+  },
 })
