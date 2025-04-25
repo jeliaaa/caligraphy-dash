fb058e30561b8c18b77b5419d2069d9a96c2fd4a
jeliaaa <julia.aleqsandre@gmail.com>
2025-04-25 22:17:51 +0400

    a

--- src/pages/RenovationSingle.tsx
+++ src/pages/RenovationSingle.tsx
@@ -44,7 +44,7 @@ const RenovationSingle = () => {
                     <svg className="w-[30px] h-[30px] rotate-180 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                         <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
                     </svg>
-                    <span className="text-lg m-0">back</span>
+                    <span className="text-lg m-0">მთავარი</span>
                 </Link>
                 <h1 className="text-center font-bold mt-2 bg-white px-10 text-3xl py-5 rounded-md shadow-md">
                     პროექტი {singleData?.id !== 0 && `: ${singleData?.track}`}
